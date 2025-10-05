e67605ac860669148edac438e193aaf697c6c7f8
Rathul2002 <rathul95@gmail.com>
2025-10-05 17:04:26 +0530

    Added Phone number

--- src/main/java/database/Testing/Test.java
+++ src/main/java/database/Testing/Test.java
@@ -2,6 +2,7 @@ package database.Testing;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Hello, Rathul!");
+        System.out.println("Phone: 1234567890");
     }
 }
